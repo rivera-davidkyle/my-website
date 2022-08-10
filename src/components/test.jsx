d32ab494c1d30d8068140aa07c4d37bba25a5e10
rivera-davidkyle <rivera.davidkyle@gmail.com>
2022-08-10 12:52:09 -0400

    Testing /w react

---
+++ src/components/test.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+    render() { 
+        return (
+        <div>
+            <h1> Hello World</h1>
+            <button type="button" class="btn btn-primary">Increment</button>
+            </div>
+            );
+    }
+}
+ 
+export default Test;
