481eeddd9215fb6b429ff78720fa5092336d014f
rivera-davidkyle <rivera.davidkyle@gmail.com>
2023-09-27 23:45:38 -0400

    First commit

--- src/components/exp.jsx
+++ src/components/exp.jsx
@@ -1,6 +1,16 @@
 import React, { Component, forwardRef } from "react";
-import { Paper, Box, Typography, Container, Grid } from "@mui/material";
+import {
+  Paper,
+  Box,
+  Typography,
+  Container,
+  Grid,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
 import "../static/css/exp.css";
+import { useState } from "react";
 import { AnimationOnScroll } from "react-animation-on-scroll";
 import alg from "../static/images/alg.png";
 import algp from "../static/images/alg_p.png";
@@ -24,13 +34,37 @@ import java from "../static/images/java.png";
 const boxStyles = {
   sx: {
     display: "flex",
-    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
     flexWrap: "wrap",
+  },
+};
+const colorScheme = {
+  sx: {
+    display: "flex",
+    flexDirection: "row",
     justifyContent: "center",
     maxHeight: "100%",
     "& > :not(style)": {
       m: 1,
       width: "auto",
+      height: "auto",
+      border: "inset rgb(129, 101, 255) 10px",
+      background: "rgb(42, 44, 80)",
+      color: "white",
+    },
+  },
+};
+
+const colorSchemeIcons = {
+  sx: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    maxHeight: "100%",
+    "& > :not(style)": {
+      m: 1,
+      width: "100%",
       height: "auto",
       border: "inset rgb(129, 101, 255) 10px",
       background: "rgb(42, 44, 80)",
@@ -57,6 +91,7 @@ const containerStyles = {
     },
   },
 };
+
 const typeStyles = {
   sx: {
     color: "rgb(129, 101, 255)",
@@ -65,125 +100,153 @@ const typeStyles = {
     padding: "5px",
   },
 };
+
 class Exp extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: "panel1",
+    };
+  }
+
+  handleChange = (panel) => (event, newExpanded) => {
+    this.setState({ expanded: newExpanded ? panel : false });
+  };
   render() {
+    const { expanded } = this.state;
     return (
       <div className="page" ref={this.props.expRef}>
         <AnimationOnScroll animateIn="animate__fadeInLeft" animateOnce="true">
-          <Grid container spacing={1}>
-            <Grid item xs={6} sx={boxStyles.sx}>
-              <Paper elevation={12} sx={{ padding: "20px" }}>
-                <Typography variant="h2" align="center">
-                  Hughes Network Systems
-                </Typography>
-                <Typography
-                  variant="h3"
-                  align="center"
-                  sx={{ color: "rgb(129, 101, 255)" }}
-                >
-                  Software Engineer
-                </Typography>
-                <Container maxWidth="md" sx={containerStyles.sx}>
-                  <img className="exp" src={alg} alt=""></img>
-                  <img className="exp" src={algp} alt=""></img>
-                </Container>
-                <Container maxWidth="sm">
-                  <Typography
-                    align="center"
-                    sx={{ fontSize: "auto", fontWeight: "5", width: "auto" }}
-                  >
-                    I have extensive experience in developing and maintaining a
-                    microservices architecture using the Django Framework,
-                    Redis, Celery, and Docker through organized team sprints and
-                    a timeline based on requirements. This architecture enabled
-                    us to store, standardize, and display atmospheric data
-                    collected from multiple EPA/NASA sites.
+          <Box sx={boxStyles.sx}>
+            <Grid container spacing={2} sx={{ width: "75%" }}>
+              <Grid item xs={12} sm={6} sx={colorScheme.sx}>
+                <Paper elevation={12} sx={{ padding: "20px" }}>
+                  <Typography variant="h2" align="center">
+                    Hughes Network Systems
                   </Typography>
-                </Container>
-              </Paper>
-              <Paper elevation={12} sx={{ padding: "20px" }}>
-                <Typography variant="h2" align="center">
-                  Atmospheric Lidar Group
-                </Typography>
-                <Typography
-                  variant="h3"
-                  align="center"
-                  sx={{ color: "rgb(129, 101, 255)" }}
-                >
-                  Software Engineer
-                </Typography>
-                <Container maxWidth="md" sx={containerStyles.sx}>
-                  <img className="exp" src={alg} alt=""></img>
-                  <img className="exp" src={algp} alt=""></img>
-                </Container>
-                <Container maxWidth="sm">
                   <Typography
+                    variant="h3"
                     align="center"
-                    sx={{ fontSize: "auto", fontWeight: "5", width: "auto" }}
+                    sx={{ color: "rgb(129, 101, 255)" }}
                   >
-                    I have extensive experience in developing and maintaining a
-                    microservices architecture using the Django Framework,
-                    Redis, Celery, and Docker through organized team sprints and
-                    a timeline based on requirements. This architecture enabled
-                    us to store, standardize, and display atmospheric data
-                    collected from multiple EPA/NASA sites.
+                    Software Engineer
                   </Typography>
-                </Container>
-              </Paper>
+                  <Container maxWidth="md" sx={containerStyles.sx}>
+                    <img className="exp" src={alg} alt=""></img>
+                    <img className="exp" src={algp} alt=""></img>
+                  </Container>
+                  <Container maxWidth="sm">
+                    <Typography
+                      align="center"
+                      sx={{ fontSize: "auto", fontWeight: "5", width: "auto" }}
+                    >
+                      I have extensive experience in developing and maintaining
+                      a microservices architecture using the Django Framework,
+                      Redis, Celery, and Docker through organized team sprints
+                      and a timeline based on requirements. This architecture
+                      enabled us to store, standardize, and display atmospheric
+                      data collected from multiple EPA/NASA sites.
+                    </Typography>
+                  </Container>
+                </Paper>
+              </Grid>
+              <Grid item xs={12} sm={6} sx={colorScheme.sx}>
+                <Paper elevation={12} sx={{ padding: "20px" }}>
+                  <Typography variant="h2" align="center">
+                    Atmospheric Lidar Group
+                  </Typography>
+                  <Typography
+                    variant="h3"
+                    align="center"
+                    sx={{ color: "rgb(129, 101, 255)" }}
+                  >
+                    Software Engineer
+                  </Typography>
+                  <Container maxWidth="md" sx={containerStyles.sx}>
+                    <img className="exp" src={alg} alt=""></img>
+                    <img className="exp" src={algp} alt=""></img>
+                  </Container>
+                  <Container maxWidth="sm">
+                    <Typography
+                      align="center"
+                      sx={{ fontSize: "auto", fontWeight: "5", width: "auto" }}
+                    >
+                      I have extensive experience in developing and maintaining
+                      a microservices architecture using the Django Framework,
+                      Redis, Celery, and Docker through organized team sprints
+                      and a timeline based on requirements. This architecture
+                      enabled us to store, standardize, and display atmospheric
+                      data collected from multiple EPA/NASA sites.
+                    </Typography>
+                  </Container>
+                </Paper>
+              </Grid>
+              <Grid item xs={12} sx={colorSchemeIcons.sx}>
+                <Paper elevation={12} sx={{ padding: "20px" }}>
+                  <Accordion
+                    expanded={expanded === "panel1"}
+                    onChange={this.handleChange("panel1")}
+                  >
+                    <AccordionSummary
+                      aria-controls="panel1d-content"
+                      id="panel1d-header"
+                    >
+                      <Typography>Collapsible Group Item #1</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Suspendisse malesuada lacus ex, sit amet blandit leo
+                        lobortis eget. Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit. Suspendisse malesuada lacus ex, sit
+                        amet blandit leo lobortis eget.
+                      </Typography>
+                    </AccordionDetails>
+                  </Accordion>
+                  <Accordion
+                    expanded={expanded === "panel2"}
+                    onChange={this.handleChange("panel2")}
+                  >
+                    <AccordionSummary
+                      aria-controls="panel2d-content"
+                      id="panel2d-header"
+                    >
+                      <Typography>Collapsible Group Item #2</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Suspendisse malesuada lacus ex, sit amet blandit leo
+                        lobortis eget. Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit. Suspendisse malesuada lacus ex, sit
+                        amet blandit leo lobortis eget.
+                      </Typography>
+                    </AccordionDetails>
+                  </Accordion>
+                  <Accordion
+                    expanded={expanded === "panel3"}
+                    onChange={this.handleChange("panel3")}
+                  >
+                    <AccordionSummary
+                      aria-controls="panel3d-content"
+                      id="panel3d-header"
+                    >
+                      <Typography>Collapsible Group Item #3</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Suspendisse malesuada lacus ex, sit amet blandit leo
+                        lobortis eget. Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit. Suspendisse malesuada lacus ex, sit
+                        amet blandit leo lobortis eget.
+                      </Typography>
+                    </AccordionDetails>
+                  </Accordion>
+                </Paper>
+              </Grid>
             </Grid>
-            <Grid item xs={6}>
-              <Container sx={boxStyles.sx}>
-              <Paper elevation={12} sx={{ padding: "20px" }}>
-                <Container>
-                  <Typography
-                    variant="h2"
-                    align="center"
-                    sx={{ padding: "10px" }}
-                  >
-                    Experienced with
-                  </Typography>
-                  <Typography align="center" sx={typeStyles.sx}>
-                    Programming Languages
-                  </Typography>
-                  <Container maxWidth="sm" sx={containerStyles.sx}>
-                    <img className="logo" src={python} alt=""></img>
-                    <img className="logo" src={java} alt=""></img>
-                    <img className="logo" src={cplus} alt=""></img>
-                    <img className="logo" src={c} alt=""></img>
-                  </Container>
-                  <Typography align="center" sx={typeStyles.sx}>
-                    Backend Technologies
-                  </Typography>
-                  <Container maxWidth="sm" sx={containerStyles.sx}>
-                    <img className="logo" src={django} alt=""></img>
-                    <img className="logo" src={mysql} alt=""></img>
-                    <img className="logo" src={redis} alt=""></img>
-                    <img className="logo" src={docker} alt=""></img>
-                  </Container>
-                  <Typography align="center" sx={typeStyles.sx}>
-                    Frontend Technologies
-                  </Typography>
-                  <Container maxWidth="sm" sx={containerStyles.sx}>
-                    <img className="logo" src={html} alt=""></img>
-                    <img className="logo" src={cs} alt=""></img>
-                    <img className="logo" src={js} alt=""></img>
-                    <img className="logo" src={reactjs} alt=""></img>
-                  </Container>
-                  <Typography align="center" sx={typeStyles.sx}>
-                    Developer Tools
-                  </Typography>
-                  <Container maxWidth="sm" sx={containerStyles.sx}>
-                    <img className="logo" src={git} alt=""></img>
-                    <img className="logo" src={atl} alt=""></img>
-                    <img className="logo" src={aws} alt=""></img>
-                    <img className="logo" src={linux} alt=""></img>
-                  </Container>
-                </Container>
-              </Paper>
-              </Container>
-            </Grid>
-          </Grid>
+          </Box>
         </AnimationOnScroll>
       </div>
     );
